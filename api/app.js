5009f0dd541b50c29b6b6c7d9fcce0bbbad6f01f
Zlaran Arnautovic <zlaya.31@gmail.com>
2020-01-13 22:15:31 +0100

    error handlers

--- api/app.js
+++ api/app.js
@@ -3,6 +3,7 @@ const helmet = require('helmet');
 
 const indexRouter = require('./routes/index');
 const searchRouter = require('./routes/search');
+const { handleError } = require('./helpers/error');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -13,6 +14,10 @@ app.use(express.json());
 
 app.use('/', indexRouter);
 app.use('/', searchRouter);
+
+app.use((err, req, res, next) => {
+    handleError(err, res);
+});
 
 app.listen(port);
 console.log('The server is listening on port 3000');
